98e3187fa6b0327d862925b7210bd68091b942b0
cashmere53 <ict.nurburgring.mikurusaders53@gmail.com>
2016-12-05 15:23:59 +0900

    create random array benchmark

---
+++ arrayBench.py
@@ -0,0 +1,33 @@
+# -*- corging: utf-8 -*-
+
+import random
+import numpy as np
+from benchmarker import Benchmarker
+
+
+SIZE = 50
+with Benchmarker(100000, width=25, cycle=3, extra=1) as bench:
+    @bench('for: random.uniform')
+    def _(bm):
+        array = []
+        append = array.append
+        ran = random.uniform
+        for i in bm:
+            child = []
+            c_append = child.append
+            for j in range(SIZE):
+                c_append(ran(-0.1, 0.1))
+            append(child)
+        np.array(array)
+
+    @bench('numpy.random.rand')
+    def _(bm):
+        array = []
+        append = array.append
+        ran = np.random.rand
+        for i in bm:
+            rarray = ran(SIZE)
+            rarray *= 2
+            rarray -= 1
+            rarray /= 10
+            append(rarray)
